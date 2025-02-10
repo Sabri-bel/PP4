8113c9080c333e50db218d68acd0718dd03706c6
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-10 17:39:51 +0100

    add Feedback model

--- wildrestaurant/settings.py
+++ wildrestaurant/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ow-33!7qq_j-)dslgxcu%*0fg1-pdx(+3b2()+y+ff#4r^5be+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
