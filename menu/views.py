8ede60e1b4326f835c6f7dee49572361a75c40d1
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-12 14:53:21 +0100

    add the POST method for the comments

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Post, Comment
+from django.contrib import messages
+from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -21,6 +23,18 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(request,
+                                 messages.SUCCESS,
+                                 'your comment is awaiting approval')
+
+    comment_form = CommentForm()
 
     return render(request,
                    "menu/post_detail.html",
@@ -28,5 +42,6 @@ def post_detail(request, slug):
                     "post": post,
                     "comments": comments,
                     "comment_count": comment_count,
+                    "comment_form": comment_form,
                   },
                  )
